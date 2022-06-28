f2f137675afab58f739b53bd384889077019c5e5
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-06-28 22:42:48 +0500

    servier running

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,11 @@
-console.log("HEllo, worlds!")
+import express from 'express'
+
+// app
+const app = express()
+
+// users
+app.use('/api/user', require('./routes/user.routes'))
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000")
+})
