1aef6bd31591cdac50c81e766bc3f14628656de7
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-07-06 17:02:10 +0500

    migrations / users - words / mysql

---
+++ backend/prisma/migrations/20220706115946_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+
+    UNIQUE INDEX `user_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `words` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `word` VARCHAR(255) NOT NULL,
+    `meaning` VARCHAR(1000) NOT NULL,
+    `synonyms` VARCHAR(1000) NOT NULL,
+    `sentence` VARCHAR(1000) NOT NULL,
+
+    UNIQUE INDEX `words_word_key`(`word`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
